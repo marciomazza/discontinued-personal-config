33ebdd306dbb956c14d2b3ad3d0922ae7af226bf
Marcio Mazza <marciomazza@gmail.com>
2015-12-05 19:30:05 -0200

    Add ssh key gen and more details to run script

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
-sudo apt-get install git
-# sudo easy_install pip
+sudo apt-get install xclip git python-pip
 sudo pip install ansible
 
+ssh-keygen -t rsa -b 4096 -C "marciomazza@gmail.com"
+xclip -sel clip < ~/.ssh/id_rsa.pub
+echo "Contents of ~/.ssh/id_rsa.pub copied to clipboard.\n Create your new ssh key on github, bitbucket, etc, then CLOSE firefox!"
+read -rsp $'Press any key to continue...\n' -n1 key
+firefox https://github.com/settings/ssh  https://bitbucket.org/account/user/mazza/ssh-keys/  https://gitlab.com/profile/keys
+
 cd ~
-git clone https://github.com/marciomazza/mazza-personal-confs.git .z
+git clone git@github.com:marciomazza/mazza-personal-config.git .z
 cd .z
 
 ansible-playbook setup.yml -i HOSTS --ask-sudo-pass
