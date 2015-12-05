676a4fd6dbf4ebaa5564cd461d02112a8dba7fa2
Marcio Mazza <marciomazza@gmail.com>
2015-12-05 09:39:45 -0200

    Init (based on JBKahn/provisioning-local)
    Based on https://github.com/JBKahn/provisioning-local

---
+++ roles/common/files/firefox/user.js
@@ -0,0 +1,7 @@
+/* Criar um link para este arquivo dentro da pasta de perfil do firefox
+ * ln -s ~/.z/firefox/user.js
+ *
+ * Esta configuracao eh para um monitor 4k pequeno (laptop lenovo)
+ */
+user_pref("layout.css.devPixelsPerPx", "2.25");
+
