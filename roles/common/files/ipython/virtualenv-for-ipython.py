cf853350966ef8b5e00b085e8857f1cb67d0d6bf
Marcio Mazza <marciomazza@gmail.com>
2015-12-06 22:50:31 -0200

    Add python configs

---
+++ roles/common/files/ipython/virtualenv-for-ipython.py
@@ -0,0 +1,19 @@
+# http://blog.ufsoft.org/2009/1/29/ipython-and-virtualenv
+
+# install:  
+# ln -s (...)/virtualenv-for-ipython.py ~/.ipython/profile_default/startup/virtualenv-for-ipython.py
+
+import site
+from os import environ
+from os.path import join
+from sys import version_info
+
+if 'VIRTUAL_ENV' in environ:
+    virtual_env = join(environ.get('VIRTUAL_ENV'),
+                       'lib',
+                       'python%d.%d' % version_info[:2],
+                       'site-packages')
+    site.addsitedir(virtual_env)
+    print 'VIRTUAL_ENV ->', virtual_env
+    del virtual_env
+del site, environ, join, version_info
