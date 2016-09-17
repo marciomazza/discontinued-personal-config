eacf386e7643b489e8e55ac17626c393e924c603
Marcio Mazza <marciomazza@gmail.com>
2016-09-17 00:33:45 -0300

    Update venv ipython support for latest realease

--- files/ipython/virtualenv-for-ipython.py
+++ files/ipython/virtualenv-for-ipython.py
@@ -1,7 +1,8 @@
 # http://blog.ufsoft.org/2009/1/29/ipython-and-virtualenv
 
-# install:  
-# ln -s (...)/virtualenv-for-ipython.py ~/.ipython/profile_default/startup/virtualenv-for-ipython.py
+# install:
+# ln -s (...)/virtualenv-for-ipython.py
+# ~/.ipython/profile_default/startup/virtualenv-for-ipython.py
 
 import site
 from os import environ
@@ -14,6 +15,6 @@ if 'VIRTUAL_ENV' in environ:
                        'python%d.%d' % version_info[:2],
                        'site-packages')
     site.addsitedir(virtual_env)
-    print 'VIRTUAL_ENV ->', virtual_env
+    print('VIRTUAL_ENV ->', virtual_env)
     del virtual_env
 del site, environ, join, version_info
