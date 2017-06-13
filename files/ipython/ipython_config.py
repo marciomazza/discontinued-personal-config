54a7e5863b3516d7b08210cf1636b2e48b55e3fa
Marcio Mazza <marciomazza@gmail.com>
2017-06-13 14:13:51 +0200

    Activate IPython autoreload by default

--- files/ipython/ipython_config.py
+++ files/ipython/ipython_config.py
@@ -1,3 +1,5 @@
 c = get_config()
 c.InteractiveShell.autocall = 1
 c.InteractiveShell.logstart = True
+c.InteractiveShellApp.extensions = ['autoreload']
+c.InteractiveShellApp.exec_lines = ['%autoreload 2']
