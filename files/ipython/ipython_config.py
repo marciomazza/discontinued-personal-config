f60f0bf9f138a36b95c16f4e7e04709831b2b83a
Marcio Mazza <marciomazza@gmail.com>
2019-05-10 10:52:33 +0200

    Add pgcli as a default venv lib and IPython magic

--- files/ipython/ipython_config.py
+++ files/ipython/ipython_config.py
@@ -1,5 +1,5 @@
 c = get_config()
 c.InteractiveShell.autocall = 1
 c.InteractiveShell.logstart = True
-c.InteractiveShellApp.extensions = ['autoreload']
-c.InteractiveShellApp.exec_lines = ['%autoreload 2']
+c.InteractiveShellApp.extensions = ["autoreload", "pgcli.magic"]
+c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
