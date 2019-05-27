fdfa4339073783fa96fd5274c574dd7c1de34ccf
Marcio Mazza <marciomazza@gmail.com>
2019-05-27 11:56:32 +0200

    Use full autocall in IPython

--- files/ipython/ipython_config.py
+++ files/ipython/ipython_config.py
@@ -1,5 +1,5 @@
 c = get_config()
-c.InteractiveShell.autocall = 1
+c.InteractiveShell.autocall = 2
 c.InteractiveShell.logstart = True
 c.InteractiveShellApp.extensions = ["autoreload", "pgcli.magic"]
 c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
