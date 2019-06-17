f8d4b1aa17b6d63680f13f8f96a0cd10d30c0061
Marcio Mazza <marciomazza@gmail.com>
2019-06-17 14:53:03 +0200

    Remove pgcli from default venv libs

--- files/ipython/ipython_config.py
+++ files/ipython/ipython_config.py
@@ -1,5 +1,5 @@
 c = get_config()
 c.InteractiveShell.autocall = 2
 c.InteractiveShell.logstart = True
-c.InteractiveShellApp.extensions = ["autoreload", "pgcli.magic"]
+c.InteractiveShellApp.extensions = ["autoreload"]
 c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
