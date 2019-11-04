4f73638196e1d9ae6efbf3b26e2a5006a20bc234
Marcio Mazza <marciomazza@gmail.com>
2019-11-04 13:43:32 +0100

    Increase default ipython terminal width

--- files/ipython/ipython_config.py
+++ files/ipython/ipython_config.py
@@ -3,3 +3,4 @@ c.InteractiveShell.autocall = 2
 c.InteractiveShell.logstart = True
 c.InteractiveShellApp.extensions = ["autoreload"]
 c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
+c.PlainTextFormatter.max_width = 110
