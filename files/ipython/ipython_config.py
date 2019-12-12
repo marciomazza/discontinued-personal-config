f89dff48a4fa75dec8304edd107dd89298a83623
Marcio Mazza <marciomazza@gmail.com>
2019-12-12 16:31:43 +0100

    temp workaround IPython for tab not working with autocall

--- files/ipython/ipython_config.py
+++ files/ipython/ipython_config.py
@@ -4,3 +4,9 @@ c.InteractiveShell.logstart = True
 c.InteractiveShellApp.extensions = ["autoreload"]
 c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
 c.PlainTextFormatter.max_width = 110
+
+# workaround for tab not working with autocall
+#   ex.: len so<TAB> => len sorted
+#   https://github.com/ipython/ipython/issues/11530
+#   https://github.com/ipython/ipython/issues/10926
+c.Completer.use_jedi = False
